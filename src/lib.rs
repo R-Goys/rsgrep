8098bdaad442b9db1c25491b1b78966ca3cf05de
rinai <rinai@g-rinai.cn>
2025-10-04 18:33:56 +0800

    perf

--- src/lib.rs
+++ src/lib.rs
@@ -49,7 +49,7 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase()
+    let query = query.to_lowercase();
     contents
         .lines()
         .filter(|line| line.to_lowercase().contains(&query))
